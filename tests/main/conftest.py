b7a8e627dc424ae57c729521dba3bccc8cf9cd88
Daniel Sanz <13658011+sdn4z@users.noreply.github.com>
2025-09-12 09:23:34 +0200

    feat: create `cli` extra group
    BREAKING CHANGE

--- tests/main/conftest.py
+++ tests/main/conftest.py
@@ -8,6 +8,6 @@ import pytest
 @pytest.fixture(scope="module")
 def disable_track() -> Generator[None, Any, None]:
     """Disables the track UI for running tests."""
-    with patch("twyn.main.track") as m_track:
+    with patch("rich.progress.track") as m_track:
         m_track.side_effect = lambda iterable, description: iterable
         yield
